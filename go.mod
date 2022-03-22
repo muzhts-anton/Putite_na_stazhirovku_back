7cf2bdf187d871a51e4dc57c3d1bbf26767874b2
Мужецкий Антон <tonich2001@gmail.com>
2022-03-22 23:10:45 +0300

    heroku go.mod

--- go.mod
+++ go.mod
@@ -1,5 +1,6 @@
 module cashmachine
 
+// +heroku goVersion go1.17
 go 1.17
 
 require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 	"os"
 )
 
-// TODO
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "https://putitenastazhirovkufront.herokuapp.com") // url to deployed front
